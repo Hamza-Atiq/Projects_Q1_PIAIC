4cec82a9c3d9ee4b2fecd315de8239741810efa1
Hamza Atiq <hamzaatiq34@gmail.com>
2024-10-30 11:37:12 +0500

    projects

--- 53_self_coding_challenges/00_intro_python/02_agreement_bot.py
+++ 53_self_coding_challenges/00_intro_python/02_agreement_bot.py
@@ -1,8 +1,6 @@
 def main():
   print('Program about Favourite animal')
-
   try:
-
     animal_name = str = input('Please enter your favourite animal ? ')
 
     print(f'My favourite animal is also {animal_name}')
